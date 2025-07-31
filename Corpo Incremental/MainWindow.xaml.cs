16a857f49eb571bb40fa7a4a04fab7ecdf8c86fe
Kosmik <malpa.w.klapkach@gmail.com>
2025-07-31 19:55:26 +0200

    Some other changes

--- Corpo Incremental/MainWindow.xaml.cs
+++ Corpo Incremental/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ namespace CorpoIncremental
 {
     public partial class MainWindow : GameWindow
     {
+
         public Game Game { get; }
 
         public MainWindow(Game game)
--- WPFGameEngine/GameWindow.cs
+++ WPFGameEngine/GameWindow.cs
@@ -1,24 +1,23 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Windows;
 
 namespace WPFGameEngine;
 
-public class GameWindow : Window
+public class GameWindow : Window, IGameWindow
 {
-	private static readonly List<GameWindow> openWindows = [];
-	public static IReadOnlyList<GameWindow> OpenWindows { get; } = openWindows.AsReadOnly();
-
 	protected override void OnContentRendered(EventArgs e)
 	{
 		base.OnContentRendered(e);
-		openWindows.Add(this);
+		IGameWindow.openWindows.Add(this);
 	}
 
 	protected override void OnClosing(CancelEventArgs e)
 	{
 		base.OnClosing(e);
-		if (openWindows.Count == 1)
+		if (IGameWindow.openWindows.Count == 1)
 		{
 			var result = MessageBox.Show("Do you really want to quit?", "Quitting", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
 			if (result == MessageBoxResult.Cancel)
@@ -30,6 +29,12 @@ public class GameWindow : Window
 	{
 		Debug.WriteLine("Close");
 		base.OnClosed(e);
-		openWindows.Remove(this);
+		IGameWindow.openWindows.Remove(this);
 	}
+}
+
+public interface IGameWindow
+{
+	protected static readonly List<GameWindow> openWindows = [];
+	public static IReadOnlyList<GameWindow> OpenWindows { get; } = openWindows.AsReadOnly();
 }
